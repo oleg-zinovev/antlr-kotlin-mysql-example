9c56780673f66bae5d1571a36736987d9973210b
ozinoviev <ozinoviev@solit-clouds.ru>
2018-10-25 19:08:01 +0300

    fixes

--- src/test/kotlin/MySqlParserTest.kt
+++ src/test/kotlin/MySqlParserTest.kt
@@ -1,6 +1,7 @@
 import org.antlr.v4.kotlinruntime.*
 import org.junit.Assert
 import org.junit.Test
+import kotlin.experimental.and
 
 /**
  * @author ozinoviev
@@ -8,8 +9,8 @@ import org.junit.Test
  */
 class MySqlParserTest {
     @Test
-    fun test_bitrix_queries_cut() {
-        val stream = CharStreams.fromStream(this.javaClass.classLoader.getResourceAsStream("bitrix_queries_cut.sql"));
+    fun test_dml_select() {
+        val stream = CharStreams.fromStream(this.javaClass.classLoader.getResourceAsStream("dml_select.sql"))
         val lexer = MySqlLexer(stream)
         val tokenStream = CommonTokenStream(lexer)
         val parser = MySqlParser(tokenStream)
@@ -33,7 +34,36 @@ class MySqlParserTest {
         Assert.assertEquals(0, errors)
         Assert.assertNotNull(root)
         Assert.assertNotNull(root.findSqlStatements())
-        Assert.assertTrue(root.findSqlStatements()!!.findSqlStatement()!!.isEmpty())
+        Assert.assertEquals(59, root.findSqlStatements()!!.findSqlStatement()!!.size)
+    }
+
+    @Test
+    fun test_bitrix_queires_cut() {
+        val stream = CharStreams.fromStream(this.javaClass.classLoader.getResourceAsStream("bitrix_queires_cut.sql"))
+        val lexer = MySqlLexer(stream)
+        val tokenStream = CommonTokenStream(lexer)
+        val parser = MySqlParser(tokenStream)
+
+        var errors = 0
+        parser.addErrorListener(object : BaseErrorListener() {
+            override fun syntaxError(recognizer: Recognizer<*, *>, offendingSymbol: Any?, line: Int, charPositionInLine: Int, msg: String, e: RecognitionException?) {
+                println(msg + line + " " + charPositionInLine)
+                errors++
+            }
+        })
+
+        lexer.addErrorListener(object : BaseErrorListener() {
+            override fun syntaxError(recognizer: Recognizer<*, *>, offendingSymbol: Any?, line: Int, charPositionInLine: Int, msg: String, e: RecognitionException?) {
+                println(msg + line + " " + charPositionInLine)
+                errors++
+            }
+        })
+
+        val root = parser.root()
+        Assert.assertEquals(0, errors)
+        Assert.assertNotNull(root)
+        Assert.assertNotNull(root.findSqlStatements())
+        Assert.assertEquals(529, root.findSqlStatements()!!.findSqlStatement()!!.size)
 
     }
 
